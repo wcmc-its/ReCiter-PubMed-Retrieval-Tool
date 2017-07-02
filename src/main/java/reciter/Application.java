70fb20925f77c4b21a42b4905811488c3329846d
jl987-Jie <jl987@cornell.edu>
2017-07-02 16:32:12 -0700

    Added Partial JSON response support

--- src/main/java/reciter/Application.java
+++ src/main/java/reciter/Application.java
@@ -2,10 +2,12 @@ package reciter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @EnableAutoConfiguration
 @ComponentScan("reciter")
+@SpringBootApplication
 public class Application {
 	
 	public static void main(String[] args) {
--- src/main/java/reciter/controller/PubMedRetrievalToolController.java
+++ src/main/java/reciter/controller/PubMedRetrievalToolController.java
@@ -1,31 +1,68 @@
 package reciter.controller;
 
 import java.io.IOException;
-import java.util.List;
+import java.net.URLEncoder;
+import java.util.*;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.MapperFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.bohnman.squiggly.Squiggly;
+import com.github.bohnman.squiggly.util.SquigglyUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import reciter.model.pubmed.PubMedArticle;
 import reciter.pubmed.retriever.PubMedArticleRetriever;
 
 @Controller
+@RequestMapping("/pubmed")
 public class PubMedRetrievalToolController {
 
 	private static final Logger slf4jLogger = LoggerFactory.getLogger(PubMedRetrievalToolController.class);
 
-	@RequestMapping(value = "/reciter/retrieve/pubmed/by/query", method = RequestMethod.GET)
-	@ResponseBody
-	public List<PubMedArticle> retrieve(@RequestParam(value="query") String query) throws IOException {
-
+	private List<PubMedArticle> retrieve(String query, String fields) throws IOException {
+		query = URLEncoder.encode(query, "UTF-8");
 		slf4jLogger.info("calling retrieve with query=[" + query + "]");
 		List<PubMedArticle> pubMedArticles = new PubMedArticleRetriever().retrievePubMed(query);
+		ObjectMapper objectMapper = Squiggly.init(new ObjectMapper(), fields);
+		List<PubMedArticle> result = new ArrayList<>();
+		objectMapper.configure(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES, true);
+		objectMapper.setSerializationInclusion(JsonInclude.Include.NON_EMPTY);
+		objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+		pubMedArticles.forEach(elem -> {
+			String partialObject = SquigglyUtils.stringify(objectMapper, elem);
+			PubMedArticle pubMedArticle = null;
+			try {
+				pubMedArticle = objectMapper.readValue(partialObject, PubMedArticle.class);
+			} catch (IOException e) {
+				slf4jLogger.error("Unable to read value from pmid=" + elem.getMedlineCitation().getMedlineCitationPMID().getPmid(), e);
+			}
+			result.add(pubMedArticle);
+		});
 		slf4jLogger.info("retrieved " + pubMedArticles.size() + " PubMed articles using query=[" + query + "]");
-		return pubMedArticles;
+		return result;
+	}
+
+	@RequestMapping(value = "/query/{query}", method = RequestMethod.GET)
+	@ResponseBody
+	public List<PubMedArticle> query(@PathVariable String query,
+									 @RequestParam(name = "fields", required = false) String fields) throws IOException {
+		return retrieve(query, fields);
+	}
+
+	@RequestMapping(value = "/pmid/{pmid}", method = RequestMethod.GET)
+	@ResponseBody
+	public List<PubMedArticle> pmid(@PathVariable long pmid, @RequestParam(name = "fields", required = false) String fields) throws RuntimeException {
+		try {
+			return retrieve(String.valueOf(pmid), fields);
+		} catch (NumberFormatException e) {
+			throw new RuntimeException("Unable to parse pmid '" + pmid + "'", e);
+		} catch (IOException e) {
+			throw new RuntimeException("IO Exception occurred.", e);
+		}
 	}
 }
