d15d13f9f4465a3d8590ae3ef08fdf2842b98c0d
sarbajitdutta <sarbajitdutta35@gmail.com>
2018-05-23 15:52:46 -0400

    implement post request for esearch

--- src/main/java/reciter/pubmed/xmlparser/PubmedESearchHandler.java
+++ src/main/java/reciter/pubmed/xmlparser/PubmedESearchHandler.java
@@ -1,16 +1,32 @@
 package reciter.pubmed.xmlparser;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.params.ClientPNames;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
 
+import reciter.pubmed.querybuilder.PubmedXmlQuery;
+
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParserFactory;
+
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A SAX handler for parsing the ESearch query from PubMed.
@@ -40,7 +56,7 @@ public class PubmedESearchHandler extends DefaultHandler {
      */
     public static PubmedESearchHandler executeESearchQuery(String eSearchUrl) {
         PubmedESearchHandler webEnvHandler = new PubmedESearchHandler();
-        InputStream inputStream = null;
+        /*InputStream inputStream = null;
         try {
             inputStream = new URL(eSearchUrl).openStream();
         } catch (IOException e) {
@@ -50,7 +66,38 @@ public class PubmedESearchHandler extends DefaultHandler {
             SAXParserFactory.newInstance().newSAXParser().parse(inputStream, webEnvHandler);
         } catch (Exception e) {
             slf4jLogger.error("Error in executeESearchQuery. url=[" + eSearchUrl + "]", e);
+        }*/
+        PubmedXmlQuery pubmedXmlQuery = new PubmedXmlQuery(eSearchUrl);
+        pubmedXmlQuery.setRetMax(1);
+        String fullUrl = pubmedXmlQuery.buildESearchQuery(); // build eSearch query.
+        slf4jLogger.info("ESearch Query=[" + fullUrl + "]");
+        try {
+		    HttpClient httpClient = HttpClients.createDefault();
+			HttpPost httppost = new HttpPost(PubmedXmlQuery.ESEARCH_BASE_URL);
+			// Request parameters and other properties.
+			List<NameValuePair> params = new ArrayList<NameValuePair>();
+			params.add(new BasicNameValuePair("db", pubmedXmlQuery.getDb()));
+			params.add(new BasicNameValuePair("retmax", String.valueOf(pubmedXmlQuery.getRetMax())));
+			params.add(new BasicNameValuePair("usehistory", pubmedXmlQuery.getUseHistory()));
+			params.add(new BasicNameValuePair("term", java.net.URLDecoder.decode(pubmedXmlQuery.getTerm(), "UTF-8")));
+			params.add(new BasicNameValuePair("retmode", pubmedXmlQuery.getRetMode()));
+			params.add(new BasicNameValuePair("retstart", String.valueOf(pubmedXmlQuery.getRetStart())));
+			httppost.setEntity(new UrlEncodedFormEntity(params));
+			httppost.setHeader("Content-Type", "application/x-www-form-urlencoded");
+			httppost.getParams().setParameter(ClientPNames.COOKIE_POLICY, "standard");
+			//Execute and get the response.
+			HttpResponse response = httpClient.execute(httppost); 
+			
+			HttpEntity entity = response.getEntity();
+			
+			if(entity != null) {
+				InputStream esearchStream = entity.getContent();
+				SAXParserFactory.newInstance().newSAXParser().parse(esearchStream, webEnvHandler);
+			}
+        } catch (SAXException | ParserConfigurationException | IOException e) {
+            slf4jLogger.error("Error parsing XML file for query=[" + eSearchUrl + "], full url=[" + fullUrl + "]", e);
         }
+        
         return webEnvHandler;
     }
 
