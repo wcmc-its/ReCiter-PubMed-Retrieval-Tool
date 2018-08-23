b4d0f85270003fcd14bbe47c1eb3b0864c657c66
sarbajitdutta <sarbajitdutta35@gmail.com>
2018-08-23 16:27:09 -0400

    alternative in handling special characters

--- src/main/java/reciter/pubmed/callable/PubMedUriParserCallable.java
+++ src/main/java/reciter/pubmed/callable/PubMedUriParserCallable.java
@@ -1,5 +1,6 @@
 package reciter.pubmed.callable;
 
+import org.apache.commons.lang3.StringEscapeUtils;
 import org.xml.sax.SAXException;
 import reciter.model.pubmed.PubMedArticle;
 import reciter.pubmed.xmlparser.PubmedEFetchHandler;
@@ -7,7 +8,17 @@ import reciter.pubmed.xmlparser.PubmedEFetchHandler;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.concurrent.Callable;
 
@@ -23,6 +34,20 @@ public class PubMedUriParserCallable implements Callable<List<PubMedArticle>> {
 
     public List<PubMedArticle> parse(String uri) throws ParserConfigurationException, SAXException, IOException {
         SAXParser saxParser = SAXParserFactory.newInstance().newSAXParser();
+        /*URL url = new URL(uri);
+        InputStream in = url.openConnection().getInputStream();
+        StringBuilder textBuilder = new StringBuilder();
+        try (Reader reader = new BufferedReader(new InputStreamReader
+          (in, Charset.forName(StandardCharsets.UTF_8.name())))) {
+            int c = 0;
+            while ((c = reader.read()) != -1) {
+            	textBuilder.append((char) c);
+            }
+        }
+        
+        String xml = textBuilder.toString().replaceAll("<sup>", "&lt;sup&gt;").replaceAll("</sup>", "&lt;/sup&gt;");
+        System.out.println(xml);
+        saxParser.parse(new ByteArrayInputStream(xml.getBytes()), xmlHandler);*/
         saxParser.parse(uri, xmlHandler);
         return xmlHandler.getPubmedArticles();
     }
