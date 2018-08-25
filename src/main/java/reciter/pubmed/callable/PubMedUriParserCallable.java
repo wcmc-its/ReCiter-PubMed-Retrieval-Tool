071cbaff859524fa348b823fcacbcc8295cac4e8
jl987-Jie <jl987@cornell.edu>
2018-08-24 21:20:53 -0700

    Singleton 'SAXParser' instead of instantiating it everytime 'PubMedUriParserCallable' is created.

--- src/main/java/reciter/pubmed/callable/PubMedUriParserCallable.java
+++ src/main/java/reciter/pubmed/callable/PubMedUriParserCallable.java
@@ -1,39 +1,23 @@
 package reciter.pubmed.callable;
 
-import org.apache.commons.lang3.StringEscapeUtils;
+import lombok.AllArgsConstructor;
 import org.xml.sax.SAXException;
 import reciter.model.pubmed.PubMedArticle;
 import reciter.pubmed.xmlparser.PubmedEFetchHandler;
 
-import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParser;
-import javax.xml.parsers.SAXParserFactory;
-
-import java.io.BufferedReader;
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.net.URL;
-import java.net.URLEncoder;
-import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.concurrent.Callable;
 
+@AllArgsConstructor
 public class PubMedUriParserCallable implements Callable<List<PubMedArticle>> {
 
     private final PubmedEFetchHandler xmlHandler;
+    private final SAXParser saxParser;
     private final String uri;
 
-    public PubMedUriParserCallable(PubmedEFetchHandler xmlHandler, String uri) {
-        this.xmlHandler = xmlHandler;
-        this.uri = uri;
-    }
-
-    public List<PubMedArticle> parse(String uri) throws ParserConfigurationException, SAXException, IOException {
-        SAXParser saxParser = SAXParserFactory.newInstance().newSAXParser();
+    public List<PubMedArticle> parse(String uri) throws SAXException, IOException {
         saxParser.parse(uri, xmlHandler);
         return xmlHandler.getPubmedArticles();
     }
