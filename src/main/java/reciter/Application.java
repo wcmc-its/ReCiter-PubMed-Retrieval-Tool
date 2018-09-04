2c912373cdb4fdc1478cfd474e079f84411d2f72
jl987-Jie <jl987@cornell.edu>
2018-09-03 22:08:48 -0700

    Add Lambda stream handler to deployment to AWS Lambda.

--- src/main/java/reciter/Application.java
+++ src/main/java/reciter/Application.java
@@ -3,12 +3,13 @@ package reciter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @EnableAutoConfiguration
 @ComponentScan("reciter")
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
