4fb6a3731d16869f7a71808f183347a99b0a1c9f
jl987-Jie <jl987@cornell.edu>
2018-09-23 14:05:29 -0700

    Remove lambda handler in favor of a proxy.

--- src/main/java/reciter/Application.java
+++ src/main/java/reciter/Application.java
@@ -3,13 +3,12 @@ package reciter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @EnableAutoConfiguration
 @ComponentScan("reciter")
 @SpringBootApplication
-public class Application extends SpringBootServletInitializer {
+public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
