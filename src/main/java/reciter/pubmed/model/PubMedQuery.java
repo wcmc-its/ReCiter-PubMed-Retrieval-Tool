d1b481a4c854467c8dc294da8e0ec28f0b9060e3
Sarbajit Dutta <sarbajitdutta35@gmail.com>
2020-08-28 10:40:00 -0400

    changed DP to EDAT for date range

--- src/main/java/reciter/pubmed/model/PubMedQuery.java
+++ src/main/java/reciter/pubmed/model/PubMedQuery.java
@@ -64,8 +64,9 @@ public class PubMedQuery {
             //parts.add(author + " [au]");
         	parts.add(author);
         }
+        //Switched from [DP] to [EDAT] for better capture of pubs : Date of publication - Date added to Entrez
         if (start != null && end != null) {
-            parts.add("(" + dt.format(start) + "[DP]" + ":" + dt.format(end) + "[DP])");
+            parts.add("(" + dt.format(start) + "[EDAT]" + ":" + dt.format(end) + "[EDAT])");
         }
         if (strategyQuery != null && !strategyQuery.isEmpty()) {
             parts.add(strategyQuery);
