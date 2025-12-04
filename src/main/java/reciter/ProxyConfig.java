4b3d409be432cd9af8cc08f1d830d632a92da445
mrj4001 <mrj4001@med.cornell.edu>
2025-12-04 17:41:01 +0530

    added proxyconfig to ensure it forwards the headers correctly

---
+++ src/main/java/reciter/ProxyConfig.java
@@ -0,0 +1,21 @@
+
+package reciter;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.web.filter.ForwardedHeaderFilter;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+
+@Configuration
+public class ProxyConfig {
+
+	@Bean
+  
+  @ConditionalOnProperty(name = "server.forward-headers-strategy", havingValue="framework")
+  @Order(Ordered.HIGHEST_PRECEDENCE)	
+  public ForwardedHeaderFilter forwardedHeaderFilter() { 
+		// This  filter ensures Spring correctly processes X-Forwarded-Host,  X-Forwarded-Proto, etc. 
+		return new ForwardedHeaderFilter(); }
+}
