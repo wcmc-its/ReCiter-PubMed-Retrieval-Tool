224d4d2e52f10f7c7573c883dc3bee9b179c2e4c
mrj4001 <mrj4001@med.cornell.edu>
2025-12-04 19:22:12 +0530

    dumping all the headers information for debugging the protocal

---
+++ src/main/java/reciter/HeaderLoggingFilter.java
@@ -0,0 +1,51 @@
+package reciter;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.Enumeration;
+
+// The @Component annotation ensures Spring automatically registers this filter
+@Component
+public class HeaderLoggingFilter implements Filter {
+
+    private static final Logger logger = LoggerFactory.getLogger(HeaderLoggingFilter.class);
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        
+        // Cast to HttpServletRequest to access header methods
+        HttpServletRequest req = (HttpServletRequest) request;
+        
+        logger.info("--- Incoming Request Header Dump ---");
+        logger.info("Request URI: {}", req.getRequestURI());
+        
+        // Iterate through all header names and log their values
+        Enumeration<String> headerNames = req.getHeaderNames();
+        while (headerNames.hasMoreElements()) {
+            String headerName = headerNames.nextElement();
+            // Logging the key header you are looking for
+            if (headerName.equalsIgnoreCase("X-Forwarded-Proto")) {
+                 logger.error("!!! CRITICAL HEADER: {} = {}", headerName, req.getHeader(headerName));
+            } else {
+                 logger.info("{}: {}", headerName, req.getHeader(headerName));
+            }
+        }
+        logger.info("----------------------------------");
+
+        // Proceed to the next filter in the chain (e.g., your controllers)
+        chain.doFilter(request, response);
+    }
+
+    // init() and destroy() methods can be left empty for a simple filter
+    @Override
+    public void init(FilterConfig filterConfig) {}
+
+    @Override
+    public void destroy() {}
+}
