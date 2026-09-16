2ed4c645e0d72ca1b04d6a48d7efad5660579991
“ved4006” <ved4006@med.cornell.edu>
2026-09-16 14:16:50 +0530

    springdoc enabled in properties file

--- src/main/java/reciter/Application.java
+++ src/main/java/reciter/Application.java
@@ -1,13 +1,9 @@
 package reciter;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.retry.annotation.EnableRetry;
 
-@EnableAutoConfiguration
-@ComponentScan("reciter")
 @EnableRetry
 @SpringBootApplication
 public class Application {
